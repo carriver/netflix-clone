2018bd62f74679d0a9eb9161ee90ab6989f68512
Carlos Rivera <carlos.rivera.link@gmail.com>
2022-01-26 00:30:34 -0500

    Implemented toast notifications to improve UI

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectUser } from '../features/userSlice';
+import { toast, ToastContainer } from 'react-toastify';
 import { auth } from '../firebase';
 import Nav from '../components/Nav';
 import Plan from '../components/Plan';
+import 'react-toastify/dist/ReactToastify.css';
 import './ProfileScreen.css';
 
 function ProfileScreen() {
@@ -22,9 +24,47 @@ function ProfileScreen() {
     );
   }, [isMobile]);
 
+  const [show, hide] = useState(true);
+
+  useEffect(() => {
+    const notify = () =>
+      toast.info('Click Netflix for Home Screen', {
+        position: 'top-left',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    notify();
+  }, []);
+
+  const [show2, hide2] = useState(true);
+
+  useEffect(() => {
+    const notify = () =>
+      toast.info('Click Avatar for Profile Screen', {
+        position: isMobile ? 'top-left' : 'top-right',
+        autoClose: 8000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    notify();
+  }, []);
+
   return (
     <div className='profileScreen'>
       <Nav />
+      <ToastContainer />
+      {show}
+      {hide}
+      <ToastContainer />
+      {show2}
+      {hide2}
       <div
         className={`${
           isMobile ? 'profileScreen__bodyMobile' : 'profileScreen__body'
