ea000e7ddcac1cd26983b5268fa72098f8356328
Carlos Rivera <carlos.rivera.link@gmail.com>
2022-01-26 23:43:35 -0500

    Added footer

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -27,6 +27,14 @@ function HomeScreen() {
       <Row title='Comedy Movies' fetchURL={requests.fetchComedyMovies} />
       <Row title='Sci-Fi Movies' fetchURL={requests.fetchScifiMovies} />
       <Row title='Thriller Movies' fetchURL={requests.fetchThrillerMovies} />
+      <br />
+      <footer className='homeScreen__footer'>
+        <h6>This website is powered by:</h6>
+        <img
+          src='https://www.themoviedb.org/assets/2/v4/logos/v2/blue_long_2-9665a76b1ae401a510ec1e0ca40ddcb3b0cfe45f1d51b77a308fea0845885648.svg'
+          alt='TMDB'
+        />
+      </footer>
     </div>
   );
 }
